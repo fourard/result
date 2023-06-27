c45e5ab723a7c77656b5b4d38601e8b1f6274173
Richard Lopes <contato.richardlopes@outlook.com>
2023-06-27 15:52:55 -0300

    feat: result map extension

--- Fourard.Result/ResultExtensions.cs
+++ Fourard.Result/ResultExtensions.cs
@@ -12,6 +12,38 @@ namespace Fourard.Result
             return GetValueOrDefault(await result);
         }
 
+        public static Result<TNewValue, TError>? Map<TValue, TError, TNewValue>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success)
+        {
+            return result switch
+            {
+                Success<TValue, TError> s => success(s.Value),
+                Failure<TValue, TError> f => f.Error,
+                Unhandled<TValue, TError> u => u.Exception,
+                _ => default
+            };
+        }
+
+        public static async Task<Result<TNewValue, TError>?> Map<TValue, TError, TNewValue>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success)
+        {
+            return Map(await result, success);
+        }
+
+        public static Result<TNewValue, TNewError>? Map<TValue, TError, TNewValue, TNewError>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
+        {
+            return result switch
+            {
+                Success<TValue, TError> s => success(s.Value),
+                Failure<TValue, TError> f => failure(f.Error),
+                Unhandled<TValue, TError> u => u.Exception,
+                _ => default
+            };
+        }
+
+        public static async Task<Result<TNewValue, TNewError>?> Map<TValue, TError, TNewValue, TNewError>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
+        {
+            return Map(await result, success, failure);
+        }
+
         public static TAction? Handle<TValue, TError, TAction>(this Result<TValue, TError>? result, Func<TValue, TAction> success, Action<TError>? failure = null, Action<Exception>? unhandled = null)
         {
             switch (result)
