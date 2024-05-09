1b9ae53280344b6eea065b416058faed626ea3c2
Richard Lopes <contato.richardlopes@outlook.com>
2024-05-09 16:36:31 -0300

    feat: disable not exhaustive switch wanings and remove nullability of mapped results

--- Fourard.Result/Result.Extensions.Mapping.cs
+++ Fourard.Result/Result.Extensions.Mapping.cs
@@ -1,50 +1,49 @@
+#pragma warning disable CS8509
+
 namespace Fourard.Result
 {
     public static partial class ResultExtensions
     {
-        public static Result<TNewValue>? Map<TValue, TNewValue>(this Result<TValue>? result, Func<TValue, TNewValue> success)
+        public static Result<TNewValue> Map<TValue, TNewValue>(this Result<TValue>? result, Func<TValue, TNewValue> success)
         {
             return result switch
             {
                 Success<TValue> s => success(s.Value),
                 Unhandled<TValue> u => u.Exception,
-                _ => default
             };
         }
 
-        public static Result<TNewValue, TError>? Map<TValue, TError, TNewValue>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success)
+        public static Result<TNewValue, TError> Map<TValue, TError, TNewValue>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success)
         {
             return result switch
             {
                 Success<TValue, TError> s => success(s.Value),
                 Failure<TValue, TError> f => f.Error,
                 Unhandled<TValue, TError> u => u.Exception,
-                _ => default
             };
         }
 
-        public static async Task<Result<TNewValue>?> Map<TValue, TNewValue>(this Task<Result<TValue>?> result, Func<TValue, TNewValue> success)
+        public static async Task<Result<TNewValue>> Map<TValue, TNewValue>(this Task<Result<TValue>?> result, Func<TValue, TNewValue> success)
         {
             return Map(await result, success);
         }
 
-        public static async Task<Result<TNewValue, TError>?> Map<TValue, TError, TNewValue>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success)
+        public static async Task<Result<TNewValue, TError>> Map<TValue, TError, TNewValue>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success)
         {
             return Map(await result, success);
         }
 
-        public static Result<TNewValue, TNewError>? Map<TValue, TError, TNewValue, TNewError>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
+        public static Result<TNewValue, TNewError> Map<TValue, TError, TNewValue, TNewError>(this Result<TValue, TError>? result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
         {
             return result switch
             {
                 Success<TValue, TError> s => success(s.Value),
                 Failure<TValue, TError> f => failure(f.Error),
                 Unhandled<TValue, TError> u => u.Exception,
-                _ => default
             };
         }
 
-        public static async Task<Result<TNewValue, TNewError>?> Map<TValue, TError, TNewValue, TNewError>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
+        public static async Task<Result<TNewValue, TNewError>> Map<TValue, TError, TNewValue, TNewError>(this Task<Result<TValue, TError>?> result, Func<TValue, TNewValue> success, Func<TError, TNewError> failure)
         {
             return Map(await result, success, failure);
         }
